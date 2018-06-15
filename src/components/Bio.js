decf81d76f1abc614341d016bd6241967963ebaf
zavolokas <sergey.zavoloka@gmail.com>
2018-06-15 09:51:44 +0200

    update Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Sergey Zavoloka`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -27,9 +27,8 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
+          Written by <strong>Sergey Zavoloka</strong>.{' '}
+          <a href="https://twitter.com/zavolokas">
             You should follow him on Twitter
           </a>
         </p>
